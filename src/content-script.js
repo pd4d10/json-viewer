6bd73cac5a54b7cefc390f13d340bff78621ccf9
Rongjian Zhang <pd4d10@gmail.com>
2018-01-28 02:57:37 +0800

    Fix save as file

--- src/content-script.js
+++ src/content-script.js
@@ -8,27 +8,44 @@ if (
       const text = document.body.childNodes[0].innerText
       JSON.parse(text)
 
-      const map = require('properties-parser').parse(
+      const localeMap = require('properties-parser').parse(
         require('devtools/client/locales/en-US/jsonview.properties')
       )
       window.JSONView = {
         json: document.body.childNodes[0],
         debug: true,
         Locale: {
-          $STR: key => {
-            try {
-              return map[key]
-            } catch (err) {
-              console.error(err)
-              return undefined
-            }
-          },
+          $STR: key => localeMap[key],
         },
         headers: {
           request: [],
           response: [],
         },
       }
+      window.addEventListener('contentMessage', e => {
+        console.log('contentMessage', e.detail)
+
+        const save = href => {
+          const link = document.createElement('a')
+          link.href = href
+
+          // /a -> a.json
+          // /a.json -> a.json
+          // /a.txt -> a.txt.json
+          let filename = location.href.split('/').slice(-1)[0]
+          if (!/\.json$/.test(filename)) {
+            filename += '.json'
+          }
+
+          link.download = filename
+          link.click()
+        }
+        switch (e.detail.type) {
+          case 'save':
+            save(e.detail.value || '')
+        }
+      })
+      // TODO: Get attributes from environment
       document.documentElement.setAttribute('platform', 'mac')
       document.documentElement.setAttribute('class', 'theme-light')
       document.documentElement.setAttribute('dir', 'ltr')
