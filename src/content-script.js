22008b4125e019f197b9bd46e6d52a6dce71d635
Rongjian Zhang <pd4d10@gmail.com>
2018-01-27 23:59:47 +0800

    Fix treeview

--- src/content-script.js
+++ src/content-script.js
@@ -8,19 +8,15 @@ if (
       const text = document.body.childNodes[0].innerText
       JSON.parse(text)
 
-      // const jsonViewStrings = require('devtools-modules/src/Services').strings.createBundle(
-      //   'chrome://devtools/locale/jsonview.properties'
-      // )
       const map = require('properties-parser').parse(
         require('devtools/client/locales/en-US/jsonview.properties')
       )
       window.JSONView = {
-        json: text,
+        json: document.body.childNodes[0],
         debug: true,
         Locale: {
           $STR: key => {
             try {
-              // return jsonViewStrings.GetStringFromName(key)
               return map[key]
             } catch (err) {
               console.error(err)
@@ -33,13 +29,10 @@ if (
           response: [],
         },
       }
-      document.write(`
-      <html platform="mac" class="theme-light" dir="ltr">
-<body>
-  <div id="content"></div>
-</body>
-</html>
-`)
+      document.documentElement.setAttribute('platform', 'mac')
+      document.documentElement.setAttribute('class', 'theme-light')
+      document.documentElement.setAttribute('dir', 'ltr')
+      document.body.innerHTML = '<div id="content"></div>'
       require('devtools/client/jsonview/css/main.css')
       require('devtools/client/jsonview/json-viewer')
     } catch (err) {
