16b3f73225b43a7c38f3c06741c10196e7545217
Rongjian Zhang <pd4d10@gmail.com>
2018-05-27 16:49:37 +0800

    Update Webpack uglify config for 4

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -11,16 +11,20 @@ module.exports = merge(config, {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
-    new UglifyJsPlugin({
-      uglifyOptions: {
-        compress: {
-          drop_console: true,
-        },
-        output: {
-          ascii_only: true, // This fix chrome.tabs.executeScript error: not utf-8
-        },
-      },
-    }),
     new BundleAnalyzerPlugin(),
   ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: true,
+          },
+          output: {
+            ascii_only: true, // This fix chrome.tabs.executeScript error: not utf-8
+          },
+        },
+      }),
+    ],
+  },
 })
