fd87c676738a1a7ee5470a0901d54e4c6fc975d1
Rongjian Zhang <pd4d10@gmail.com>
2021-07-31 23:03:01 +0800

    chore: add script to get available locales

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -7,172 +7,10 @@ import rename from 'gulp-rename'
 import gulpif from 'gulp-if'
 import replace from 'gulp-replace'
 import plumber from 'gulp-plumber'
-
-// https://hg.mozilla.org/l10n-central
-// [].map.call($('table').querySelectorAll('td:first-child'), (v) => v.innerText).slice(1)
-const locales = [
-  'ace',
-  'ach',
-  'af',
-  'ak',
-  'an',
-  'ar',
-  'as',
-  'ast',
-  'az',
-  'be',
-  'bg',
-  'bn',
-  'bn-BD',
-  'bn-IN',
-  'bo',
-  'br',
-  'brx',
-  'bs',
-  'ca',
-  'ca-valencia',
-  'cak',
-  'ckb',
-  'crh',
-  'cs',
-  'csb',
-  'cy',
-  'da',
-  'de',
-  'dsb',
-  'el',
-  'en-CA',
-  'en-GB',
-  'en-ZA',
-  'eo',
-  'es-AR',
-  'es-CL',
-  'es-ES',
-  'es-MX',
-  'et',
-  'eu',
-  'fa',
-  'ff',
-  'fi',
-  'fr',
-  'frp',
-  'fur',
-  'fy-NL',
-  'ga-IE',
-  'gd',
-  'gl',
-  'gn',
-  'gu-IN',
-  'gv',
-  'he',
-  'hi-IN',
-  'hr',
-  'hsb',
-  'hto',
-  'hu',
-  'hy-AM',
-  'hye',
-  'ia',
-  'id',
-  'ilo',
-  'is',
-  'it',
-  'ixl',
-  'ja',
-  'ja-JP-mac',
-  'ka',
-  'kab',
-  'kk',
-  'km',
-  'kn',
-  'ko',
-  'kok',
-  'ks',
-  'ku',
-  'lb',
-  'lg',
-  'lij',
-  'lo',
-  'lt',
-  'ltg',
-  'lv',
-  'mai',
-  'meh',
-  'mix',
-  'mk',
-  'ml',
-  'mn',
-  'mr',
-  'ms',
-  'my',
-  'nb-NO',
-  'ne-NP',
-  'nl',
-  'nn-NO',
-  'nr',
-  'nso',
-  'ny',
-  'oc',
-  'or',
-  'pa-IN',
-  'pai',
-  'pbb',
-  'pl',
-  'ppl',
-  'pt-BR',
-  'pt-PT',
-  'quy',
-  'qvi',
-  'rm',
-  'ro',
-  'ru',
-  'rw',
-  'sah',
-  'sat',
-  'sc',
-  'scn',
-  'sco',
-  'si',
-  'sk',
-  'sl',
-  'son',
-  'sq',
-  'sr',
-  'ss',
-  'st',
-  'sv-SE',
-  'sw',
-  'szl',
-  'ta',
-  'ta-LK',
-  'te',
-  'tg',
-  'th',
-  'tl',
-  'tn',
-  'tr',
-  'trs',
-  'ts',
-  'tsz',
-  'uk',
-  'ur',
-  'uz',
-  've',
-  'vi',
-  'wo',
-  'xcl',
-  'xh',
-  'zam',
-  'zh-CN',
-  'zh-TW',
-  'zu',
-]
+import fetch from 'node-fetch'
+import cheerio from 'cheerio'
 
 const color = 'rgba(135, 135, 137, 0.9)'
-
-export const clean = () => {
-  return del('./vendor')
-}
 
 export const downloadGeckoZip = () => {
   return download({
@@ -181,8 +19,23 @@ export const downloadGeckoZip = () => {
   }).pipe(gulp.dest('.'))
 }
 
+let locales = []
+
 export const downloadL10n = gulp.series(
-  locales.map((locale) => (cb) => {
+  async (cb) => {
+    const html = await fetch('https://hg.mozilla.org/l10n-central').then(
+      (res) => res.text()
+    )
+    const $ = cheerio.load(html)
+    locales = $('table')
+      .first()
+      .find('td:first-child')
+      .map((i, el) => $(el).text().trim())
+      .toArray()
+    // console.log(locales)
+    cb()
+  },
+  ...locales.map((locale) => (cb) => {
     return download({
       file: `${locale}.properties`,
       url: `https://hg.mozilla.org/l10n-central/${locale}/raw-file/tip/devtools/client/jsonview.properties`,
@@ -197,28 +50,31 @@ export const downloadL10n = gulp.series(
   })
 )
 
-export function gecko() {
-  return gulp
-    .src('./gecko.zip')
-    .pipe(unzip())
-    .pipe(
-      rename((obj) => {
-        obj.dirname = obj.dirname.replace(/mozilla-central-.*?\//, 'gecko/')
-      })
-    )
-    .pipe(
-      gulpif(
-        (file) => file.path.endsWith('arrow.svg'),
-        replace(/fill="(.*?)"/, `fill="${color}"`)
+export const gecko = gulp.series(
+  () => {
+    return del('./vendor/gecko')
+  },
+  () => {
+    return gulp
+      .src('./gecko.zip')
+      .pipe(unzip())
+      .pipe(
+        rename((obj) => {
+          obj.dirname = obj.dirname.replace(/mozilla-central-.*?\//, 'gecko/')
+        })
       )
-    )
-    .pipe(
-      gulpif(
-        (file) => file.path.endsWith('filter-small.svg'),
-        replace(/<svg/, `<svg fill="${color}"`)
+      .pipe(
+        gulpif(
+          (file) => file.path.endsWith('arrow.svg'),
+          replace(/fill="(.*?)"/, `fill="${color}"`)
+        )
       )
-    )
-    .pipe(gulp.dest('./vendor'))
-}
-
-export default gulp.series(clean, gecko)
+      .pipe(
+        gulpif(
+          (file) => file.path.endsWith('filter-small.svg'),
+          replace(/<svg/, `<svg fill="${color}"`)
+        )
+      )
+      .pipe(gulp.dest('./vendor'))
+  }
+)
