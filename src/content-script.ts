d3efef9c539e5a9d97b4461aea5fdf121f2cb359
rachpt <rachpt@126.com>
2024-03-26 20:55:36 +0800

    feat: support edge browser (#29)

--- src/content-script.ts
+++ src/content-script.ts
@@ -3,12 +3,23 @@ import { logDebug } from './utils'
 
 // https://github.com/pd4d10/json-viewer/issues/28
 // Since Chrome 117
-const el = document.body.lastElementChild
+const elPre = document.querySelector('body > pre')
 
-if (el instanceof HTMLElement && el?.tagName === 'PRE') {
+// Edge Browser
+// https://learn.microsoft.com/en-us/microsoft-edge/devtools-guide-chromium/json-viewer/json-viewer
+const elDiv = document.querySelector('body > div[hidden=true]')
+
+if (elDiv instanceof HTMLElement) {
   try {
-    JSON.parse(el.innerText) // check if it's valid JSON
-    render(el.innerText)
+    JSON.parse(elDiv.innerText) // check if it's valid JSON
+    render(elDiv.innerText)
+  } catch (err) {
+    logDebug(err)
+  }
+} else if (elPre instanceof HTMLElement) {
+  try {
+    JSON.parse(elPre.innerText) // check if it's valid JSON
+    render(elPre.innerText)
   } catch (err) {
     logDebug(err)
   }
